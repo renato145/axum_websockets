023c00b0ac79993e1db4ada95dfae6189d5d3d7f
renato <renato.hermoza@pucp.edu.pe>
2021-09-27 15:15:43 +0930

    pass python_repo tests

--- src/subsystems/python_repo.rs
+++ src/subsystems/python_repo.rs
@@ -5,6 +5,20 @@ use glob::glob;
 use serde::Deserialize;
 use std::path::Path;
 
+#[derive(thiserror::Error)]
+pub enum PythonRepoError {
+    #[error("Invalid path: {0:?}")]
+    InvalidPath(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for PythonRepoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
 pub struct PythonRepoSystem;
 
 #[async_trait::async_trait]
@@ -27,20 +41,6 @@ impl Subsystem for PythonRepoSystem {
     }
 }
 
-#[derive(thiserror::Error)]
-pub enum PythonRepoError {
-    #[error("Invalid path: {0:?}")]
-    InvalidPath(String),
-    #[error(transparent)]
-    UnexpectedError(#[from] anyhow::Error),
-}
-
-impl std::fmt::Debug for PythonRepoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
 #[derive(Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Task {
@@ -49,9 +49,9 @@ pub enum Task {
 
 #[tracing::instrument(name = "GetFiles task")]
 fn get_files(payload: serde_json::Value) -> Result<serde_json::Value, PythonRepoError> {
-    let path = payload.to_string();
-    if !Path::new(&path).exists() {
-        return Err(PythonRepoError::InvalidPath(path));
+    let path = payload.as_str().unwrap_or("");
+    if !Path::new(path).exists() {
+        return Err(PythonRepoError::InvalidPath(path.into()));
     }
 
     let files = glob(&format!("{}/**/*.py", path))
